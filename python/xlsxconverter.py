af9e830a483fdee753d96efd3151fd1bbc918b31
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2021-01-08 14:07:40 +0200

    tweaked python converter

--- python/xlsxconverter.py
+++ python/xlsxconverter.py
@@ -1,6 +1,5 @@
-import urllib.request
+from urllib.request import urlretrieve
 from openpyxl import load_workbook
-from datetime import date
 import json
 import re
 
@@ -12,25 +11,23 @@ data = {}
 data["drinks"] = []
 
 print("Downloading file")
-urllib.request.urlretrieve(table_url, filename_in)
+urlretrieve(table_url, filename_in)
 print("Download ready")
 
 wb = load_workbook(filename_in, data_only=True)
-ws = wb["Alkon Hinnasto Tekstitiedostona"]
+ws = wb.worksheets[0] #tämä lataa ensimmäisen sivun excel-tiedostosta
 
 def compute_img_url(name, id):
-    url = "https://images.alko.fi/images/cs_srgb,f_auto,t_products/cdn/934332/santa-julia-organic-malbec-2019.jpg"
-
     name = re.sub(r'([^\s\w]|_)+', '', name)
     name = name.replace("ä", "a")
     name = name.replace("ö", "o")
     name = name.replace("å", "a")
     name = name.replace(" ", "-")
 
-    baseUrl = "https://images.alko.fi/images/cs_srgb,f_auto,t_products/cdn/"+str(id)+"/"+name+".jpg"
-    print(baseUrl)
-    return baseUrl
+    url = "https://images.alko.fi/images/cs_srgb,f_auto,t_products/cdn/"+str(id)+"/"+name+".jpg"
+    return url
 
+calc = 0
 for row in ws.iter_rows(min_row=5, max_col=22, values_only=True):
     if row[3]:
         id = row[0]
@@ -44,7 +41,7 @@ for row in ws.iter_rows(min_row=5, max_col=22, values_only=True):
         imgUrl = compute_img_url(name, id)
 
         ppL = 0
-        ppLe = "approaches infinity" #vanha: ppLe = 0
+        ppLe = "approaches infinity"
 
         if float(volume) != 0:
             ppL = round(float(price)/float(volume), 2)
@@ -64,7 +61,12 @@ for row in ws.iter_rows(min_row=5, max_col=22, values_only=True):
             "url": url,
             "imgUrl": imgUrl
         }
+
         data["drinks"].append(drink)
+        print("handling drink id "+str(id))
+        calc += 1
 
 with open(filename_out, "w") as outfile:
     json.dump(data, outfile)
+
+print(str(calc)+" drinks found")
