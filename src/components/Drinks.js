97ec769838f965ecd1da1acfab99a2ce602363f9
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2021-01-10 13:31:55 +0200

    update todo and styles

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -3,29 +3,56 @@ import { useDispatch, useSelector } from "react-redux"
 import InfiniteScroll from "react-infinite-scroll-component"
 import { orderDrinks } from "../selectors/drinkSelector"
 import { setListSize } from "../reducers/listSizeReducer"
+import "../styles/drinkStyle.css"
 
 const Drink = ({ drink }) => {
     return (
-        <tr>
-            <td>
+        <div className="drinkContainer">
+            <div className="drinkChildDiv">
                 <img
+                    className="drinkImg"
                     src={drink.imgUrl}
                     alt={drink.name}
                     crossOrigin="anonymous"
                 />
-            </td>
-            <td>
-                <div>id: {drink.id}</div>
-                <div>name: {drink.name}</div>
-                <div>price: {drink.price}€</div>
-                <div>alcohol: {drink.alcohol}%</div>
-                <div>manufacturer: {drink.manufacturer}</div>
-                <div>volume: {drink.size} L</div>
-                <div>price per L of ethanol: {drink.priceperethanolL}{isNaN(drink.priceperethanolL) ? "":"€"}</div>
-                <a href={drink.url} rel="noreferrer" target="_blank">Alko</a>
-                <div>type: {drink.type}</div>
-            </td>
-        </tr>
+            </div>
+            <div className="drinkChildDiv">
+                <table className="drinkTable">
+                    <tbody>
+                        <tr>
+                            <td>name:</td>
+                            <td>{drink.name}</td>
+                        </tr>
+                        <tr>
+                            <td>price:</td>
+                            <td>{drink.price.toFixed(2)}€</td>
+                        </tr>
+                        <tr>
+                            <td>alcohol:</td>
+                            <td>{drink.alcohol}%</td>
+                        </tr>
+                        <tr>
+                            <td>manufacturer:</td>
+                            <td>{drink.manufacturer}</td>
+                        </tr>
+                        <tr>
+                            <td>volume:</td>
+                            <td>{drink.size} L</td>
+                        </tr>
+                        <tr>
+                            <td>price per L of ethanol:</td>
+                            <td>
+                                {isNaN(drink.priceperethanolL) ? drink.priceperethanolL : drink.priceperethanolL.toFixed(2)}{isNaN(drink.priceperethanolL) ? "":"€"}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>type:</td>
+                            <td>{drink.type}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
@@ -40,28 +67,20 @@ const Items = () => {
         dispatch(setListSize(currentListSize + 20))
     }
 
-    const listStyle = {
-        marginBottom: "40px"
-    }
-
     return (
-        <div style={listStyle}>
+        <div id="drinkList">
             <InfiniteScroll
                 dataLength={sortedDrinks.length}
                 next={showMoreDrinks}
                 hasMore={true}
                 endMessage={<h1>this is the end</h1>}
             >
-                <table>
-                    <tbody>
-                        {sortedDrinks.map(d => 
-                            <Drink 
-                                drink={d}
-                                key={d.id}
-                            />  
-                        )}
-                    </tbody>
-                </table>
+                {sortedDrinks.map(d => 
+                    <Drink 
+                        drink={d}
+                        key={d.id}
+                    />  
+                )}
             </InfiniteScroll>
         </div>
     )
